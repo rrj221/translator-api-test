e8b2cb8282474cd1a234108cdb3191821d128a86
Ryan Jarrell <rrj221@gmail.com>
2016-10-01 17:02:10 -0400

    added another news search with more parameters

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,16 @@ router.get('/news', function(req, res) {
 	    console.log(result.status, result.headers, result.body);
 	    res.send(result.body);
 	});
+});
+
+router.get('/news/more-advanced', function(req, res) {
+	//do a search for articles from german in german for 'syria' do not include discussions and limit to one day ago
+	unirest.get("https://webhose.io/search?token=62bbc47b-2985-4449-ae34-54b112e8108c&format=json&q=syria%20language%3A(german)%20thread.country%3ADE%20(site_type%3Anews%20OR%20site_type%3Ablogs)&ts=1475268929067")
+	.header("Accept", "text/plain")
+	.end(function (result) {
+	    console.log(result.status, result.headers, result.body);
+	    res.send(result.body);
+	});
 })
 
 module.exports = router;
