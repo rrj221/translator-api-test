19b6621cba1920897fdbe4252c5d7e1aa268e5cc
Ryan Jarrell <rrj221@gmail.com>
2016-10-01 16:34:51 -0400

    added news

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios');
+var unirest = require('unirest');
 var translate = require('yandex-translate')('trnsl.1.1.20161001T021445Z.1b7df2e80a455a66.3994daff49605b8440ef8870db72268bebd2b91a');
 
 /* GET home page. */
@@ -12,6 +12,17 @@ router.get('/text', function(req, res) {
 	// res.render('index', { title: 'Express' });
 	translate.translate('You can burn my house, steal my car, drink my liquor from an old fruitjar.', { to: 'ru' }, function(err, response) {
 	  res.send(response.text);
+	});
+});
+
+router.get('/news', function(req, res) {
+
+	//search for news with query of 'election'
+	unirest.get("https://webhose.io/search?token=62bbc47b-2985-4449-ae34-54b112e8108c&format=json&q=election")
+	.header("Accept", "text/plain")
+	.end(function (result) {
+	    console.log(result.status, result.headers, result.body);
+	    res.send(result.body);
 	});
 })
 
